78741bcb753200e84111cd13f5ba02dfa7128091
whalid <whalidashif@gmail.com>
2021-12-06 02:56:30 +0600

    checking branch

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 public class hello{
     public static void main(string[] args){
         System.out.println("hello");
+        System.out.println("shifat");
+        System.out.println("ashif");
         greeting("ashif");
     }
     public static void greeting(string name){
