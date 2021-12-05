d2f55468f51dda705121ab3372e34bc5483dd8fa
whalid <whalidashif@gmail.com>
2021-12-06 02:41:02 +0600

    chaging code

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class hello{
     public static void main(string[] args){
         System.out.println("hello");
+        greeting("ashif");
+    }
+    public static void greeting(string name){
+        System.out.println("hello"+name);
     }
 
 
